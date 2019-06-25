3b5ea55744f08a1017164b5d650d8416e3f6807d
Devit951 <devit951@gmail.com>
2019-06-25 12:59:52 +0500

    Added volumebar view

--- app/src/main/java/com/github/devit951/volumebar/MainActivity.kt
+++ app/src/main/java/com/github/devit951/volumebar/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.github.devit951.volumebar
 
+import android.content.Context
+import android.media.AudioManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var audioManager: AudioManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        vv_bar.calibrate(
+                maxVolumeCount = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                currentVolumeCount = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
+        if (event?.keyCode == KeyEvent.KEYCODE_VOLUME_UP || event?.keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+            vv_bar.setCurrentVolume(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC))
+        }
+        return super.dispatchKeyEvent(event)
     }
 }
--- volumeview/src/main/java/com/github/devit951/volumeview/VolumeView.kt
+++ volumeview/src/main/java/com/github/devit951/volumeview/VolumeView.kt
@@ -1,4 +1,69 @@
 package com.github.devit951.volumeview
 
-class VolumeView:  {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+class VolumeView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyle: Int = 0): View(context, attrs, defStyle){
+
+    private val rectPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GRAY
+    }
+
+    private val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+    }
+
+
+    private var maxVolumeCount = 0
+    private var currentVolumeCount = 0
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawRoundRect(0f, 0f, width.toFloat(), height.toFloat(), dp2Px(10f).toFloat(), height / 2f, rectPaint)
+        drawCircle(canvas)
+    }
+
+    @SuppressLint("SwitchIntDef")
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = when(View.MeasureSpec.getMode(widthMeasureSpec)){
+            View.MeasureSpec.EXACTLY -> View.MeasureSpec.getSize(View.MeasureSpec.getMode(widthMeasureSpec))
+            else -> dp2Px(200f)
+        }
+        val height = when(View.MeasureSpec.getMode(heightMeasureSpec)){
+            View.MeasureSpec.EXACTLY -> View.MeasureSpec.getSize(View.MeasureSpec.getMode(heightMeasureSpec))
+            else -> dp2Px(20f)
+        }
+        setMeasuredDimension(width, height)
+    }
+
+    fun calibrate(maxVolumeCount: Int, currentVolumeCount: Int){
+        this.maxVolumeCount = maxVolumeCount
+        this.currentVolumeCount = currentVolumeCount
+        invalidate()
+    }
+
+    fun setCurrentVolume(streamVolume: Int) {
+        currentVolumeCount = streamVolume
+        invalidate()
+    }
+
+    private fun drawCircle(canvas: Canvas){
+        val radius = height / 2f
+        var circleX = (width / maxVolumeCount) * currentVolumeCount.toFloat()
+        if (circleX == 0f){
+            circleX += radius
+        } else if (circleX + radius >= width) {
+            circleX -= (radius - 10)
+        }
+        canvas.drawCircle(circleX, height / 2f, height / 2f, circlePaint)
+    }
+
+    private fun dp2Px(dp: Float): Int = (Resources.getSystem().displayMetrics.density * dp).toInt()
 }
